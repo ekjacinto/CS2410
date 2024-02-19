ca7ed3799d10ad461d50a4a16d86bbdf7d8f42b5
Ethan <ethankylejacinto@gmail.com>
2024-02-18 19:18:08 -0800

    feat: added documentation + adjusted file opening

--- Program1/main.py
+++ Program1/main.py
@@ -1,10 +1,20 @@
 """
-  FUNCTIONS:
-    getHighestRainfall(param: dict) => return: tuple
-    getSmallestRainfall(param: dict) => return: tuple
-    getMeanRainfall(param: dict) => return: float
-    getCitiesGreaterMean(param: dict, float) => return: int
-    main()
+    FUNCTIONS:
+        getHighestRainfall(@param: dict) => @return: tuple
+        getSmallestRainfall(@param: dict) => @return: tuple
+        getMeanRainfall(@param: dict) => @return: float
+        getCitiesGreaterMean(@param: dict, float) => @return: int
+        main()
+"""
+
+""" getHighestRainfall(collection)
+    Get the highest rainfall amount from rainfall.txt
+
+    Keyword arguments:
+    collection -- dictionary of cities to rainfall amount values
+
+    Returns value:
+    highestRainfallLocation, highestRainfall -- tuple of the city and highest rainfall amount
 """
 
 
@@ -18,6 +28,17 @@ def getHighestRainfall(collection):
     return highestRainfallLocation, highestRainfall
 
 
+""" getLowestRainfall(collection)
+    Get the lowest rainfall amount from rainfall.txt
+
+    Keyword arguments:
+    collection -- dictionary of cities to rainfall amount values
+
+    Return value:
+    lowestRainfallLocation, lowestRainfall -- tuple of the city and lowest rainfall amount
+"""
+
+
 def getLowestRainfall(collection):
     lowestRainfall = list(collection.values())[0]
     lowestLocationLocation = list(collection.keys())[0]
@@ -26,6 +47,17 @@ def getLowestRainfall(collection):
             lowestRainfall = rainfall_amt
             lowestLocationLocation = city
     return lowestLocationLocation, lowestRainfall
+
+
+""" getMeanRainfall(collection)
+    Get the mean rainfall amount from rainfall.txt
+
+    Keyword arguments:
+    collection -- dictionary of cities to rainfall amount values
+
+    Return value:
+    meanRainfall -- floating point value of the sum of rainfall values with the total number of cities
+"""
 
 
 def getMeanRainfall(collection):
@@ -37,6 +69,18 @@ def getMeanRainfall(collection):
     return meanRainfall
 
 
+""" getNumCitiesGreaterThanMean(collection, mean)
+    Get the number of cities greater than the mean rainfall amount from rainfall.txt
+
+    Keyword arguments:
+    collection -- dictionary of cities to rainfall amount values
+    mean -- the mean rainfall amount value
+
+    Return value:
+    numCities -- the number of cities greater than the mean rainfall amount
+"""
+
+
 def getNumCitiesGreaterThanMean(collection, mean):
     numCities = 0
     LISTOFRAINFALLVALUES = list(collection.values())
@@ -46,25 +90,34 @@ def getNumCitiesGreaterThanMean(collection, mean):
     return numCities
 
 
+""" main()
+    Reads rainfall.txt, prints multi-line string of cities with converted cm rainfall values, stores 
+    city and corresponding rainfall value in a dictionary, calls defined functions for console output
+"""
+
+
 def main():
     fileContents = ""
     fileContents_in_cm = ""
     rainfall_collections = {}
     try:
-        with open("./rainfall.txt", "r") as f:
-            # store file contents in var fileContents
-            fileContents = f.read()
+        file = open("./rainfall.txt", "r")
+        # store file contents in var fileContents
+        fileContents = file.read()
 
-            # use dict to correspond place with rainfall amount
-            for line in fileContents.split("\n"):
-                place_to_rainfall = line.split(" ")
+        # use dict to correspond place with rainfall amount
+        for line in fileContents.split("\n"):
+            place_to_rainfall = line.split(" ")
 
-                # convert rainfall from inches to cm
-                RAINFALL_IN_CM = round(float(place_to_rainfall[1]) * 2.54, 2)
-                # correspond key=place to value=rainfall_amt
-                rainfall_collections[place_to_rainfall[0]] = RAINFALL_IN_CM
-                # print to console new format
-                fileContents_in_cm += f"{place_to_rainfall[0]} {rainfall_collections[place_to_rainfall[0]]}\n"
+            # convert rainfall from inches to cm
+            RAINFALL_IN_CM = round(float(place_to_rainfall[1]) * 2.54, 2)
+            # correspond key=place to value=rainfall_amt
+            rainfall_collections[place_to_rainfall[0]] = RAINFALL_IN_CM
+            # print to console new format
+            fileContents_in_cm += (
+                f"{place_to_rainfall[0]} {rainfall_collections[place_to_rainfall[0]]}\n"
+            )
+        file.close()
     except FileNotFoundError:
         print("File not found")
 
